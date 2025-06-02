228532ae2f4d9b11b19bbc63bbf4719ead17dec7
= <byteblazedev@gmail.com>
2025-06-02 23:40:56 +0530

    initialize

---
+++ strings/strings.rkt
@@ -0,0 +1,9 @@
+(string-append "Ada" " " "Lovelace") ; Ada Lovelace
+(string-length "appleman")  ;8
+
+(substring "supercow" 2 5) ;per
+
+(substring "1234567" 2 6) ;3456
+(substring "0123456" 2 6)  ;2345
+
+
